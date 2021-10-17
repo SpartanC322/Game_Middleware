8bbae85e3c55f9df5a5d90033551d570a0ff1ac1
Ben Lenihan <benlenihan99@gmail.com>
2021-10-17 04:15:38 +0100

    Signed-off-by: Ben Lenihan <benlenihan99@gmail.com>

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public List<Ball> allSpheres;
+    public List<Plane> allPlanes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allPlanes = FindObjectsOfType<Plane>().ToList();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        allSpheres = FindObjectsOfType<Ball>().ToList();
+
+        for (int i = 0; i < allSpheres.Count;i++)
+        {
+            Ball firstSphere = allSpheres[i];
+            for (int j = i + 1; j < allSpheres.Count;j++)
+            {
+                Ball secondSphere = allSpheres[j];
+
+                if (firstSphere.CollidesWith(secondSphere))
+                {
+                    ResolveCollision(firstSphere, secondSphere);
+
+                    //Debug.Log("Sphere collision");
+                }
+            }
+
+            foreach (Plane p in allPlanes)
+            {
+                if (firstSphere.CollidesWith(p))
+                {
+                    ResolveCollision(firstSphere, p);
+
+                    //Debug.Log("Plane Collision");
+                }
+            }
+        }
+    }
+
+    private void ResolveCollision(Ball firstSphere, Plane p)
+    {
+        //Version 1
+        //transform.position -=  velocity * Time.deltaTime;
+
+
+        //float overlap = radius - pl.distanceTo(transform.position);
+        //transform.position += overlap * pl.normal;
+
+        Vector3 v_s, v_c;
+
+        v_s = Gp.perpendicular(firstSphere.velocity, p.normal);
+
+        v_c = Gp.parallel(firstSphere.velocity, p.normal);
+
+        float time_r = (firstSphere.radius - p.distanceTo(transform.position)) / v_c.magnitude;
+
+        transform.position += firstSphere.velocity * time_r;
+
+
+
+        firstSphere.velocity = v_s - (v_c * firstSphere.CoR);
+
+
+        transform.position -= firstSphere.velocity * time_r;
+
+        //velocity = -velocity * CoR;//Coeficient of Restitution
+    }
+
+    private static void ResolveCollision(Ball firstSphere, Ball secondSphere)
+    {
+        //Debug.Log("collided");
+
+        Vector3 distanceNorm = (secondSphere.transform.position - firstSphere.transform.position).normalized;
+
+        Vector3 v_s, v_c, v_s2, v_c2;
+
+        v_s = Gp.perpendicular(firstSphere.velocity, distanceNorm);
+
+        v_c = Gp.parallel(firstSphere.velocity, distanceNorm);
+
+
+        v_s2 = Gp.perpendicular(secondSphere.velocity, distanceNorm);
+
+        v_c2 = Gp.parallel(secondSphere.velocity, distanceNorm);
+
+
+        v_c = (((firstSphere.mass - secondSphere.mass) / (firstSphere.mass + secondSphere.mass)) * v_c) + (((secondSphere.mass * 2) / (firstSphere.mass + secondSphere.mass)) * v_c2);
+
+        v_c2 = (((secondSphere.mass - firstSphere.mass) / (firstSphere.mass + secondSphere.mass)) * v_c2) + (((firstSphere.mass * 2) / (firstSphere.mass + secondSphere.mass)) * v_c);
+
+        firstSphere.velocity = v_s + (v_c * firstSphere.CoR);
+
+        secondSphere.velocity = v_s2 + (v_c2 * firstSphere.CoR);
+    }
+}
---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    Vector3 point 
+    { 
+        get { return transform.position; }
+        set { transform.position = value; } 
+    }
+
+    public Vector3 normal
+    {
+        get { return transform.up; }
+        set { transform.up = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public float distanceTo(Vector3 v)
+    {
+        Vector3 vtop = point - v;
+
+        return Gp.parallel(vtop, normal).magnitude;
+    }
+
+    //internal bool CollidesWith(Ball firstSphere)
+    //{
+    //    return distanceTo(firstSphere.transform.position) < firstSphere.radius;
+    //}
+}
