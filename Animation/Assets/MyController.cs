d7f15af2611a23a8355012e0a8be1fad8fbcc4c3
STUDENT Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>
2021-10-18 10:43:08 +0100

    Signed-off-by: STUDENT Benjamin M. Lenihan <Benjamin.M.Lenihan@students.ittralee.ie>

--- Animation/Assets/MyController.cs
+++ Animation/Assets/MyController.cs
@@ -6,7 +6,7 @@ public class MyController : MonoBehaviour
 {
     Animator newAnimation;
     float velocity = 0;
-    public float acceleration = 0.1f, deceleration = 0.1f;
+    public float acceleration = 0.1f, deceleration = 0.1f, turn = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +19,28 @@ public class MyController : MonoBehaviour
     {
         bool forwardPress = Input.GetKey(KeyCode.W);
         bool runPress = Input.GetKey(KeyCode.LeftShift);
+        bool stop = Input.GetKey(KeyCode.Q);
+        bool turnLeft = Input.GetKey(KeyCode.A);
+        bool turnRight = Input.GetKey(KeyCode.D);
 
         if (forwardPress && velocity < 1)
         {
             newAnimation.SetBool("Walk", true);
-            
         }
         
         if (!forwardPress)
         {
             newAnimation.SetBool("Walk", false);
+        }
+
+        if(turnLeft)
+        {
+            turn -= acceleration * Time.deltaTime;
+        }
+
+        if(turnRight)
+        {
+            turn += acceleration * Time.deltaTime;
         }
 
         if (runPress && forwardPress)
@@ -46,11 +58,12 @@ public class MyController : MonoBehaviour
             velocity = 0;
         }
         
-        if(velocity > 0.1f)
+        if(velocity > 0.2f && !stop)
         {
-            velocity = 0.1f;
+            velocity = 0.2f;
         }
 
+        newAnimation.SetFloat("Turn", turn);
         newAnimation.SetFloat("Velocity", velocity);
     }
 }
