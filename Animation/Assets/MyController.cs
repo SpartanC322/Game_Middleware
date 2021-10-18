07280a758febbde349ecfcdadb7ddde539fc2f3a
Ben Lenihan <benlenihan99@gmail.com>
2021-10-18 04:42:48 +0100

    Animation
    Walk to Run Blend Tree

--- Animation/Assets/MyController.cs
+++ Animation/Assets/MyController.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class MyController : MonoBehaviour
 {
-    Animator NewAnimation;
+    Animator newAnimation;
+    float velocity = 0;
+    public float acceleration = 0.1f, deceleration = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        NewAnimation = gameObject.GetComponent<Animator>();
-       //  
+        newAnimation = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        bool forwardPress = Input.GetKey(KeyCode.W);
+        bool runPress = Input.GetKey(KeyCode.LeftShift);
+
+        if (forwardPress && velocity < 1)
         {
-            NewAnimation.SetBool("Run", true);
+            newAnimation.SetBool("Walk", true);
+            
         }
+        
+        if (!forwardPress)
+        {
+            newAnimation.SetBool("Walk", false);
+        }
+
+        if (runPress && forwardPress)
+        {
+            velocity += acceleration * Time.deltaTime;
+        }
+
+        if(!runPress)
+        {
+            velocity -= deceleration * Time.deltaTime;
+        }
+
+        if(velocity < 0)
+        {
+            velocity = 0;
+        }
+        
+        if(velocity > 0.1f)
+        {
+            velocity = 0.1f;
+        }
+
+        newAnimation.SetFloat("Velocity", velocity);
     }
 }
